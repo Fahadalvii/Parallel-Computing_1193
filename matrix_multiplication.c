bb528a369d99a6c102ac2c6014dcdc5a59cb5cd7
Fahadalvii <muhammadfahadalvintucs@gmail.com>
2025-03-19 10:42:11 +0500

    Update code for Sequential Parallel with Chunk Size

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,40 +1,42 @@
 #include <stdio.h>
 #include <omp.h>
+#include <stdlib.h>
 
-#define SIZE 520
+#define SIZE 700
 
 int main()
 {
     int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
     int i, j, k;
 
-    printf("matrices with sample values"); 
+    
+    int num_threads =4; 
+    omp_set_num_threads(num_threads);
+
+    // Initialize matrices
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
-            A[i][j] = i + j;  
-            B[i][j] = i - j;  
+            A[i][j] = i + j;
+            B[i][j] = i - j;
+            C[i][j] = 0;
         }
     }
 
-    
     double start = omp_get_wtime();
     
-    // Matrix multiplication
+    // Parallel matrix multiplication with chunking
+    #pragma omp parallel for private(i, j, k) schedule(static, 1000000)
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < SIZE; j++) {
-            C[i][j] = 0;
             for (k = 0; k < SIZE; k++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 
-    
     double end = omp_get_wtime();
-
     
-    double time_taken = end - start;
-    printf("Sequential Execution Time: %f seconds\n", time_taken);
+    printf("Parallel Execution Time: %f seconds using %d threads\n", end - start, num_threads);
 
     return 0;
 }
